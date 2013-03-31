8fe0b18a1208a973e6e4ca3ac842003c0a90ec7e
Derrek Bertrand <bernard.neurotic@gmail.com>
2013-03-31 16:28:34 -0400

    Updates.

--- main.c
+++ main.c
@@ -16,23 +16,31 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_native_dialog.h>
 
-#define TILE_WIDTH 8 //number of pixels w & h a tile is
-#define TILE_SIZE TILE_WIDTH*TILE_WIDTH*sizeof(uint32_t) //size of a tile's data in bytes
+//number of pixels w & h a tile is
+#define TILE_WIDTH 8
+//size of a tile's data in bytes
+#define TILE_SIZE TILE_WIDTH*TILE_WIDTH*sizeof(uint32_t)
 
 //macros for getting the local origins
-
 #define origin_x(i, w) (i*TILE_WIDTH%w)
 #define origin_y(i, w) ((i/(w/TILE_WIDTH))*TILE_WIDTH)
 
 typedef struct tile {
-    uint32_t hash[4]; //hash of tile
-    uint32_t index; //index of tile in the original file
+    uint32_t hash; //hash of tile used for comparisons
+    uint32_t data[TILE_WIDTH][TILE_WIDTH]; //raw data to be written back to a file later
+    void* next; //pointer to the next tile in the list
 } tile;
 
 //not worth making a header for one big-ass function
 //a function ripped off stack overflow that hashes data
 uint32_t lookup3 (const void *key, size_t length, uint32_t initval);
 
+void disp_help()
+{
+    printf("This is the help for the program, but it ain't that helpful.\n");
+}
+
+//Initializes allegro and its addons returns 0 if all is okay
 int init_alleg()
 {
     if(!al_init())
@@ -47,96 +55,15 @@ int init_alleg()
         return 1;
     }
 
-    if(!al_init_native_dialog_addon())
-    {
-        printf("Al Init Dialogs failed!\n");
-        return 1;
-    }
-
     return 0;
 }
 
-//allows the user to select a bitmap with a dialog
-//loads the image and returns it, but only
-//if it is divisible by tile width (which we need for our purposes)
-ALLEGRO_BITMAP* get_image()
+void shutdown_alleg()
 {
-    ALLEGRO_BITMAP* source_img = NULL;
-    const char* value = NULL;
-    ALLEGRO_FILECHOOSER* filechooser = NULL;
-
-    filechooser = al_create_native_file_dialog("", "Choose Source Image", "*.png", ALLEGRO_FILECHOOSER_FILE_MUST_EXIST | ALLEGRO_FILECHOOSER_PICTURES);
-
-    //we have a sensible dialog
-    if(filechooser != NULL)
-    {
-        if(al_show_native_file_dialog(NULL, filechooser))
-        {
-            if(al_get_native_file_dialog_count(filechooser) == 1)
-            {
-                //we have a path
-                value = al_get_native_file_dialog_path(filechooser, 0);
-
-                //we aren't concerned with displaying; we're concerned with
-                //manipulating the image in memory
-                al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
-
-                //load the img
-                source_img = al_load_bitmap(value);
-
-                //check to see if it loaded
-                //if not, go ahead and quit here
-                if(source_img == NULL)
-                {
-                    al_show_native_message_box(
-                        NULL,
-                        "Error",
-                        "Error",
-                        "That is not a valid PNG file.",
-                        NULL,
-                        ALLEGRO_MESSAGEBOX_ERROR
-                    );
-                    return NULL;
-                }
-
-                //its nice that we have an image, but we are a bit more particular than that
-                //it needs to be divisible by tile size
-                if(al_get_bitmap_width(source_img) % TILE_WIDTH)
-                {
-                    al_show_native_message_box(
-                        NULL,
-                        "Error",
-                        "Error",
-                        "The image is not divisible by tile width!",
-                        NULL,
-                        ALLEGRO_MESSAGEBOX_ERROR
-                    );
-                    al_destroy_bitmap(source_img);
-                    return NULL;
-                }
-
-                if(al_get_bitmap_height(source_img) % TILE_WIDTH)
-                {
-                    al_show_native_message_box(
-                        NULL,
-                        "Error",
-                        "Error",
-                        "The image is not divisible by tile height!",
-                        NULL,
-                        ALLEGRO_MESSAGEBOX_ERROR
-                    );
-                    al_destroy_bitmap(source_img);
-                    return NULL;
-                }
-            }
-        }
-        //clean up the file dialog
-        al_destroy_native_file_dialog(filechooser);
-    }
-    return source_img;
+    return;
 }
 
-void output_image(ALLEGRO_BITMAP* src, int w, int h, tile* hashes, int n_unique)
+/*void output_image(ALLEGRO_BITMAP* src, int w, int h, tile* hashes, int n_unique)
 {
     ALLEGRO_BITMAP* dst = NULL;
 
@@ -167,85 +94,187 @@ void output_image(ALLEGRO_BITMAP* src, int w, int h, tile* hashes, int n_unique)
     al_set_target_bitmap(NULL);
     //destroy
     al_destroy_bitmap(dst);
-}
+}*/
 
-int rip_tiles(ALLEGRO_BITMAP* src)
+//the current version that is being worked on
+//takes a linked list of tiles and returns
+//the number of tiles added
+int rip_tiles(tile* tile_list, char* filename)
 {
-    int unique = 1;
-    int i = 0; //index of the tile
-    int j = 0; //index of the max unique hashes (to be used with hashes)
-    int k = 0; //iterator for use with j
-    int temp = 0; //temp storage for hash
-    int row = 0;
-    int col = 0; //row and column
-    int w = al_get_bitmap_width(src);
-    int h = al_get_bitmap_height(src);
-    int n_tiles = (w/TILE_WIDTH) * (h/TILE_WIDTH); //the number of tiles
-    int data[TILE_WIDTH][TILE_WIDTH]; //the data in the tiles
-    unsigned char r, g, b, a; //holders for the color elements
+    //the source image
+    ALLEGRO_BITMAP* img = NULL;
+    ALLEGRO_LOCKED_REGION* region = NULL;
+    //iterators
+    uint32_t i;
+    //row & col
+    uint32_t row = 0;
+    uint32_t col = 0;
+    //width and height
+    uint32_t w, h;
+    //number of tiles in the image
+    uint32_t n_tiles = 0;
+    //number of unique tiles
+    uint32_t tile_count = 0;
+    //the four hashes
+    uint32_t hashes[4];
+    //the raw data to be hashed in a contiguous region
+    uint32_t data[TILE_WIDTH][TILE_WIDTH];
+    //the offset into data
+    int offset;
+    unsigned char* region_data;
 
-    tile* hashes = (tile*)al_malloc(sizeof(tile)*n_tiles);
-    memset ((void*)hashes, 0, sizeof(hashes));
+    printf("Ripping tiles from %s\n", filename);
 
-    //iterate through tiles
+    al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
+    //try to load the bmp
+    img = al_load_bitmap(filename);
+
+    if(img == NULL)
+        return 0;
+
+    w = al_get_bitmap_width(img);
+    h = al_get_bitmap_height(img);
+
+    if(w % TILE_WIDTH)
+        return 0;
+
+    if(h % TILE_WIDTH)
+        return 0;
+
+    //get the number of tiles
+    n_tiles = (w/TILE_WIDTH) * (h/TILE_WIDTH);
+    //lock the region - er, well... the whole thing
+    region = al_lock_bitmap(img, ALLEGRO_PIXEL_FORMAT_ARGB_8888,ALLEGRO_LOCK_READONLY);
+    region_data = (unsigned char*)region->data;
+
+    //we now apparently have an image worth working on
+    //iterate through each tile
     for(i = 0;i < n_tiles;i++)
     {
         //dump bitmap data into an array
         for(row = 0;row < TILE_WIDTH;row++)
             for(col = 0;col < TILE_WIDTH;col++)
             {
-                al_unmap_rgba(al_get_pixel(src, origin_x(i,w) + col, origin_y(i,w) + row), &r, &g, &b, &a);
-                data[row][col] = r | (g << 8) | (b << 16) | (a << 24);
+                offset = (region->pitch*(origin_y(i,w)+7-row))+(sizeof(uint32_t)*(origin_x(i,w)+col));
+                data[row][col] = region_data[offset];
+                printf("%d",data[row][col]);
             }
+        hashes[1] = lookup3((const void*)data,sizeof(data),12345);
 
-        temp = lookup3((const void*)data,sizeof(data),12345);
-        unique = 1;
-        //check vs all the other unique ones found so far
-        for(k = 0; k < j;k++)
-        {
-            if(hashes[k].hash[0] == temp)
+        /*for(row = 0;row < TILE_WIDTH;row++)
+            for(col = 0;col < TILE_WIDTH;col++)
             {
-                //not unique!
-                unique = 0;
+                data[row][col] = (uint32_t)(region->data[(region->pitch*(origin_y(i,w)+row))+(sizeof(uint32_t)*(origin_x(i,w)+7-col))]);
+            }
+        hashes[2] = lookup3((const void*)data,sizeof(data),12345);
+
+        for(row = 0;row < TILE_WIDTH;row++)
+            for(col = 0;col < TILE_WIDTH;col++)
+            {
+                data[row][col] = (uint32_t)(region->data[(region->pitch*(origin_y(i,w)+7-row))+(sizeof(uint32_t)*(origin_x(i,w)+7-col))]);
+            }
+        hashes[3] = lookup3((const void*)data,sizeof(data),12345);
+
+        for(row = 0;row < TILE_WIDTH;row++)
+            for(col = 0;col < TILE_WIDTH;col++)
+            {
+                data[row][col] = (uint32_t)(region->data[(region->pitch*(origin_y(i,w)+row))+(sizeof(uint32_t)*(origin_x(i,w)+col))]);
+            }
+        hashes[0] = lookup3((const void*)data,sizeof(data),12345);
+
+        tile* tile = tile_list;
+        while(1)
+        {
+            //if it matches any of the 4
+            //then its not unique, so quit
+            if(hashes[0] == tile->hash)
+                break;
+            if(hashes[1] == tile->hash)
+                break;
+            if(hashes[2] == tile->hash)
+                break;
+            if(hashes[3] == tile->hash)
+                break;
+            
+            //alright this tile passed the tests
+            //and the next one don't exist
+            if(tile->next == NULL)
+            {
+                //that means its unique
+                //allocate a tile and set next to it
+                tile->next = al_malloc(sizeof(tile));
+                //zero it out
+                memset((void*)tile->next, 0,sizeof(tile));
+                //copy one of the hashes over
+                tile->next->hash = hashes[0];
+                //copy the data
+                memcpy((void*)tile->next->data, (const void*)data,sizeof(data));
+
+                //we found a tile
+                tile_count++;
                 //break the loop
                 break;
             }
+            //not the end try the next one
+            tile = tile->next;
         }
-
-        //if its still considered unique
-        if(unique)
-        {
-            //add the hash and index
-            hashes[j].hash[0] = temp;
-            hashes[j].index = i;
-            j++;   //we now have one more unique one
-        }
+        //next tile!*/
     }
 
-    output_image(src, w, h, hashes, j);
-    al_destroy_bitmap(src);
-    al_free(hashes);
-    return 0;
+    //unlock bitmap
+    al_unlock_bitmap(img);
+    //destroy the bitmap
+    al_destroy_bitmap(img);
+
+    return tile_count;
 }
 
-int main()
+int main(int nargs, char* args[])
 {
-    ALLEGRO_BITMAP* src_img = NULL;
+    //iterators
+    uint32_t i;
+    //the number of images passed to the program
+    uint32_t n_images = 0;
+    //the number of tiles we have found
+    uint32_t n_tiles = 0;
+    //pointer to the tile list
+    tile* tile_list = NULL;
 
-    if(init_alleg())
-        return 1;
-    
-    while(1)
+    //we loop through the images until we see
+    //an argument or the end of the list
+    for(i = 0;i < nargs;i++)
     {
-        src_img = get_image();
-
-        if(src_img == NULL)
+        //if the first character is a dash
+        //then thats the start of the argument list
+        if(args[i][0] == '-')
             break;
 
-        rip_tiles(src_img);
+        //we have this many images
+        n_images = i;
     }
 
+    //display help and exit if no imgs
+    if(n_images == 0)
+    {
+        disp_help();
+        return 0;
+    }
+
+    //initialize allegro
+    if(init_alleg())
+        return 1;
+
+    //we have images apparently
+    //go through and rip the tiles from each
+    //of the images and keep track of the tile count
+    for(i = 0;i < n_images;i++)
+        n_tiles += rip_tiles(tile_list, args[i]);
+
+
+    //generate the dump file
+    //dump_tiles(n_tiles, tile_list);
+
     //shut down
-    al_shutdown_native_dialog_addon();
+    shutdown_alleg();
     return 0;
 }
